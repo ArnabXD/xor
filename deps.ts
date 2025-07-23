5d3f107bc7911db0cbb76d46db83651333ce125d
ArnabXD <arnabxd@pm.me>
2025-07-23 23:23:46 +0530

    fix: correct import path for mtkruto filters

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // Internal dependencies
 export { cleanEnv, makeValidator, num, str } from "envalid";
-export * from "@mtkruto/filters";
+export * from "$mtkruto-filters";
 export * from "@mtkruto/parse";
